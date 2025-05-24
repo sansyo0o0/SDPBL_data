679313e4af7959d837f08ecc08f706189302cc3c
OgataDaichi56 <g2431013@g.tcu.ac.jp>
2025-05-24 23:20:46 +0900

    Add files via upload

---
+++ q2.js
@@ -0,0 +1,23 @@
+let code = "ENIGMA";
+
+const input = document.getElementById("codeInput");
+const defuseBtn = document.getElementById("defuseBtn");
+const message = document.getElementById("message");
+
+
+function checkCode() {
+  if (input.value == code) {
+    message.textContent = "✅ 解除成功！";
+    input.disabled = true;
+    defuseBtn.disabled = true;
+
+    // 正解なら t1.html に遷移
+    setTimeout(() => {
+      window.location.href = "t1.html";
+    }, 1000); // メッセージ表示のために少し待機（任意）
+  } else {
+    window.location.href = "f1.html";
+  }
+}
+
+defuseBtn.addEventListener("click", checkCode);
